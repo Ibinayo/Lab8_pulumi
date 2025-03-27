5d766dc833aa601669f6d185d33f63e50b221c8e
Ibinayo <113690479+Ibinayo@users.noreply.github.com>
2025-03-26 20:21:01 -0400

    Update index.ts

--- ppinfra/index.ts
+++ ppinfra/index.ts
@@ -13,8 +13,8 @@ const bucket = new aws.s3.BucketV2("bucket");
 
 const bucketWebsite = new aws.s3.BucketWebsiteConfigurationV2("bucketWebsite", {
     bucket: bucket.bucket,
-    indexDocument: { key: indexDocument },  // FIXED: Changed 'suffix' to 'key'
-    errorDocument: { suffix: errorDocument },  // FIXED: Changed 'key' to 'suffix'
+    indexDocument: { suffix: indexDocument },  // FIXED: Changed 'suffix' to 'key'
+    errorDocument: { key: errorDocument },  // FIXED: Changed 'key' to 'suffix'
 });
 
 // Configure ownership controls for the new S3 bucket
